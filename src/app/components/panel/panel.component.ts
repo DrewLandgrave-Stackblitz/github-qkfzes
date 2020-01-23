aba6d2d321c1b40cb367831b6dc0dbb3797b3668
Landgrave, Drew <drew.landgrave@cbsinteractive.com>
2020-01-23 14:06:59 -0500

    Fixing interface

--- src/app/components/panel/panel.component.ts
+++ src/app/components/panel/panel.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit, Input, ViewChild, ViewContainerRef } from '@angular/core';
+import { CustomComponent } from 'src/app/interfaces/custom-component.interface';
 
 @Component({
   selector: 'advtech-panel',
   templateUrl: './panel.component.html',
   styleUrls: ['./panel.component.scss']
 })
-export class PanelComponent implements OnInit, ComponentBuilder {
+export class PanelComponent implements OnInit, CustomComponent {
   
   @ViewChild('insertionPoint', { read: ViewContainerRef, static: true })
   @Input() public insertionPoint: ViewContainerRef;
